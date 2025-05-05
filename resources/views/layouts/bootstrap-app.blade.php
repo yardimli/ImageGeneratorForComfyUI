9801e391e5870245ca1c3cffc7b194aa9b5716bd
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-05-05 12:16:56 +0800

    favicon

--- resources/views/layouts/bootstrap-app.blade.php
+++ resources/views/layouts/bootstrap-app.blade.php
@@ -15,6 +15,11 @@
 	<link rel="dns-prefetch" href="//fonts.bunny.net">
 	<link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 	
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	
 	@yield('styles')
 
 </head>
