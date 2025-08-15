07e5c74d8723154ed4dda70246d74e82798e61b2
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-08-15 12:41:52 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 	use App\Http\Controllers\Api\PromptApiController;
+	use App\Http\Controllers\UpscaleAndNotesController;
 	use Illuminate\Http\Request;
 	use Illuminate\Support\Facades\Route;
 
