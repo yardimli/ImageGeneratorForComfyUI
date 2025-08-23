02fa912c586e9f447fe456d110e9e26a144e1d2e
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-08-23 23:43:55 +0800

    Update CreateStoryController.php

--- app/Http/Controllers/CreateStoryController.php
+++ app/Http/Controllers/CreateStoryController.php
@@ -14,6 +14,7 @@
 	use Illuminate\Support\Facades\File;
 	use Illuminate\Support\Facades\Log;
 	use Illuminate\Support\Facades\Validator;
+	use Illuminate\Validation\Rule;
 	use Illuminate\Validation\ValidationException;
 
 	class CreateStoryController extends Controller
